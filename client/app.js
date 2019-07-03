cee9af51d3511280743c02cdd3b58f56d34e4e3c
Brooke <brooke.snellings@gmail.com>
2019-07-03 16:32:30 -0500

    Initialize

---
+++ client/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h1>Lets Get Started Baby</h1>;
+  }
+}
+export default App;
