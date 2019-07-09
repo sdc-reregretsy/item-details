d4862e001fc174f5bad4e6a73973776b47e91a18
Brooke <brooke.snellings@gmail.com>
2019-07-08 21:11:03 -0500

    Add Docker

--- server/server.js
+++ server/server.js
@@ -2,9 +2,16 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 3001;
+const axios = require("axios");
 
 app.use(express.json());
 app.use(express.static("dist"));
+
+app.get('/', (req, res) => {
+  res.send();
+})
+
+axios.get('')
 
 app.listen(port, () => {
   console.log(`The shenanigans have started on aisle ${port}`);
